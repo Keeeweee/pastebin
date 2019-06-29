d10df7d76e1a6bc87330cead605e7ff5c6a8c01a
Keewee <epsilonmajorquezero@gmail.com>
2019-06-29 03:33:14 +0200

    Add function to save user when creating snippet

--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,15 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
